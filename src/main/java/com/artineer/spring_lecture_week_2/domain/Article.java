56653584201d0e94574cdd3b823d4f2789752ff0
rhqudco <rhqudco1204@naver.com>
2021-11-28 23:39:53 +0900

    temp

---
+++ src/main/java/com/artineer/spring_lecture_week_2/domain/Article.java
@@ -0,0 +1,43 @@
+package com.artineer.spring_lecture_week_2.domain;
+
+import com.artineer.spring_lecture_week_2.dto.ArticleDto;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String title;
+    String content;
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public static Article of(ArticleDto.ReqPost from) {
+        return Article.builder()
+                .title(from.getTitle())
+                .content(from.getContent())
+                .build();
+    }
+
+    public static Article of(ArticleDto.ReqPut from, Long id) {
+        return Article.builder()
+                .id(id)
+                .title(from.getTitle())
+                .content(from.getContent())
+                .build();
+    }
+}
